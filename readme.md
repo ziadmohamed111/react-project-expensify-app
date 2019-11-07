b4085b4ab23a850d1558296fd3aaae54d33b804b
zeyadMohamed <ziadm3950@gmail.com>
2019-11-07 04:59:53 +0200

    Setup production webpack build

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Git commands
+
+git init  - creat a new git repo
+git status - view changes to your repo
+git add - add files to staging area
+git commit - creat new commit with files from staging area
+git log - view latest commits
--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import AppRouter from "./routers/AppRouter" 
 import { Provider } from 'react-redux'
 import configureStore from "./store/configureStore"
-import {addExpense} from './actions/expenses';
 import "./styles/style.scss"
 import "normalize.css/normalize.css"
 import 'react-dates/lib/css/_datepicker.css';
